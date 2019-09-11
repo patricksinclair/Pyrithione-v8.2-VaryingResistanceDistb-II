b1e814c4db2e45a07a7519167a063880c82986f0
Patrick Sinclair <patricksinclair@hotmail.co.uk>
2019-09-11 13:09:58 +0100

    12 sept run

--- src/PyrithioneMain.java
+++ src/PyrithioneMain.java
@@ -4,7 +4,7 @@ public class PyrithioneMain {
         double scale_99 = 2.71760274, sigma_99 = 0.56002833;
         double scale_95 = 1.9246899, sigma_95 = 1.00179994;
         double scale_90 = 1.01115312, sigma_90 = 1.51378016;
-        String date = "-20-August-2019";
+        String date = "-12-September-2019";
         String folderID99 = "-99_suscep"+date;
         String folderID95 = "-95_suscep"+date;
         String folderID90 = "-90_suscep"+date;
@@ -13,6 +13,6 @@ public class PyrithioneMain {
         int nReps = 200;
         System.out.println("geno distbs now with thickness limit and directory creation");
 
-        BioSystem.getEventCountersAndRunPopulations(nReps, scale_95, sigma_95, folderID95);
+        BioSystem.getEventCountersAndRunPopulations(nReps, scale_99, sigma_99, folderID99);
     }
 }
